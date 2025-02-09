a2cbd71c587d49a0dd70a3960db1c6b49e146ac1
silvermete0r <gregormelon.2003@gmail.com>
2025-02-09 23:44:23 +0500

    quotes-feature-upd+more_quotes

--- pages/quotes.js
+++ pages/quotes.js
@@ -44,6 +44,94 @@ const quotes = [
   {
     text: "The only limit to our realization of tomorrow will be our doubts of today.",
     author: "Franklin D. Roosevelt"
+  },
+  {
+    text: "The best way to predict the future is to create it.",
+    author: "Peter Drucker"
+  },
+  {
+    text: "Ever tried. Ever failed. No matter. Try Again. Fail again. Fail better.",
+    author: "Samuel Beckett"
+  },
+  {
+    text: "Either you run the day or the day runs you.",
+    author: "Jim Rohn"
+  },
+  {
+    text: "It always seems impossible until it's done.",
+    author: "Nelson Mandela"
+  },
+  {
+    text: "It does not matter how slowly you go as long as you do not stop.",
+    author: "Confucius"
+  }, 
+  {
+    text: "He who conquers himself is the mightiest warrior.",
+    author: "Confucius"
+  }, 
+  {
+    text: "Love your family, work super hard, live your passion.",
+    author: "Gary Vaynerchuk" 
+  },
+  {
+    text: "You are never too old to set another goal or to dream a new dream.",
+    author: "C.S. Lewis"
+  },
+  {
+    text: "Try to be a rainbow in someone's cloud.",
+    author: "Maya Angelou"
+  },
+  {
+    text: "Give light and people will find the way.",
+    author: "Ella Baker"
+  },
+  {
+    text: "A problem is a chance for you to do your best.",
+    author: "Duke Ellington"
+  },
+  {
+    text: "Life is short. Don’t be lazy.",
+    author: "Sophia Amoruso"
+  },
+  {
+    text: "Do one thing every day that scares you.",
+    author: "Eleanor Roosevelt"
+  },
+  {
+    text: "Whatever you are, be a good one.",
+    author: "Abraham Lincoln"
+  },
+  {
+    text: "Hold the vision, trust the process.",
+    author: "Unknown"
+  },
+  {
+    text: "The hard days are what make you stronger.",
+    author: "Aly Raisman"
+  },
+  {
+    text: "Keep your eyes on the stars, and your feet on the ground.",
+    author: "Theodore Roosevelt"
+  },
+  {
+    text: "Hard work beats talent when talent doesn’t work hard.",
+    author: "Tim Notke"
+  },
+  {
+    text: "Don’t limit your challenges. Challenge your limits.",
+    author: "Unknown"
+  },
+  {
+    text: "Focus on being productive instead of busy.",
+    author: "Tim Ferriss"
+  },
+  {
+    text: "Consistency is what transforms average into excellence.",
+    author: "Unknown"
+  },
+  {
+    text: "Success is not always about greatness. It’s about consistency.",
+    author: "Dwayne Johnson"
   }
 ];
 
@@ -63,6 +151,7 @@ export default function Quotes() {
         <div className={styles.quotesList}>
           {quotes.map((quote, index) => (
             <blockquote key={index} className={styles.quoteItem}>
+              <span className={styles.quoteId}>#{index + 1}</span>
               <q>{quote.text}</q>
               <cite>{quote.author}</cite>
             </blockquote>
